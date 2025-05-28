3147e85acc12d9e04fce01b2ebebc9588e310553
limitedcoffeetime <neroscientist29@gmail.com>
2025-05-28 15:17:35 -0700

    basic nav template

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style = {styles.text}> About screen </Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -12,10 +12,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
-    color: '#fff'
-  }
-})
+    color: '#fff',
+  },
+});
