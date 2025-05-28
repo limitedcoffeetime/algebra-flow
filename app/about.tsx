a0b2eb23646eec4839b2770aec2ad141f84612de
limitedcoffeetime <neroscientist29@gmail.com>
2025-05-28 13:32:26 -0700

    template

--- app/about.tsx
+++ app/about.tsx
@@ -1,10 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style = {styles.text}> Hello Algebra </Text>
-
+      <Text style = {styles.text}> About screen </Text>
     </View>
   );
 }
