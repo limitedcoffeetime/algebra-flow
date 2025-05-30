6e899c239509d29d4fffb43fe399e0f5a098217c
limitedcoffeetime <neroscientist29@gmail.com>
2025-05-30 14:05:19 -0700

    package update

--- components/ProblemContainer.tsx
+++ components/ProblemContainer.tsx
@@ -23,7 +23,7 @@ const ProblemContainer = ({ problem }: Props) => {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 13,
+    padding: 10,
     borderRadius: 10,
     backgroundColor: '#f0f0f0', // Light grey background
     alignItems: 'center',
